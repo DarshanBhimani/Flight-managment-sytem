85458ea34e119b407bca87d7a5e90f8e7d54caad
Manoj-Suru <manojsuru002@gmail.com>
2021-10-28 22:13:46 +0530

    Hi

--- ManageHeader.h
+++ ManageHeader.h
@@ -1,6 +1,15 @@
 void Insert()
 {
+    static int i = 0;
+    flights = (struct Airplane*)malloc(30*sizeof(struct Airplane));
+    printf("Enter the name of the Flight ");
+    gets((flights+i)->Name);
 
+    printf("Enter the capacity of the flight ");
+    scanf("%d",&(flights+i)->capacity);
+
+    printf("Enter the arrival time of flight ");
+    
 }
 
 void Delete()
